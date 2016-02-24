ca1f93727e7be4db62c8e1b7329eb4a42a09c76c
Dave Hunt <dave.hunt@gmail.com>
2016-02-24 17:17:13 +0000

    Update to pytest-selenium 1.2.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,9 @@ import requests
 
 
 @pytest.fixture(scope='session')
-def capabilities(capabilities):
-    capabilities.setdefault('tags', []).append('treeherder')
-    return capabilities
+def session_capabilities(session_capabilities):
+    session_capabilities.setdefault('tags', []).append('treeherder')
+    return session_capabilities
 
 
 @pytest.fixture
