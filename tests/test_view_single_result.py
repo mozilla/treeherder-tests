5cffe80a6f7c95179110bba8d5346eb5c67629e0
rebeccabillings <rbillings@mozilla.com>
2016-04-21 14:28:59 -0700

    Removed test classes

--- tests/test_view_single_result.py
+++ tests/test_view_single_result.py
@@ -7,13 +7,11 @@ import pytest
 from pages.treeherder import TreeherderPage
 
 
-class TestSingleResult:
-
-    @pytest.mark.nondestructive
-    def test_open_single_result(self, base_url, selenium):
-        page = TreeherderPage(base_url, selenium).open()
-        assert len(page.result_sets) > 1
-        datestamp = page.result_sets[0].datestamp
-        page.result_sets[0].view()
-        assert 1 == len(page.result_sets)
-        assert datestamp == page.result_sets[0].datestamp
+@pytest.mark.nondestructive
+def test_open_single_result(base_url, selenium):
+    page = TreeherderPage(base_url, selenium).open()
+    assert len(page.result_sets) > 1
+    datestamp = page.result_sets[0].datestamp
+    page.result_sets[0].view()
+    assert 1 == len(page.result_sets)
+    assert datestamp == page.result_sets[0].datestamp
