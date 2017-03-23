c89648fdc4c3f0b07f67d8cd57042209845be027
Rebecca Billings <rbillings@mozilla.com>
2017-03-23 10:00:43 -0700

    Fix test_treeherder_dropdown by removing superfluous asserts (#124)

--- tests/test_treeherder_menu.py
+++ tests/test_treeherder_menu.py
@@ -12,13 +12,9 @@ def test_treeherder_dropdown(base_url, selenium):
     """Switch between Treeherder and Perfherder using header dropdown"""
     treeherder_page = TreeherderPage(selenium, base_url).open()
 
-    # Verify Treeherder loads
-    assert treeherder_page.unclassified_failure_count > 0
-
     # Switch to Perfherder page
     perfherder_page = treeherder_page.open_perfherder_page()
     assert perfherder_page.is_graph_chooser_displayed
 
     # Return to Treeherder page
     treeherder_page = perfherder_page.open_treeherder_page()
-    assert treeherder_page.unclassified_failure_count > 0
